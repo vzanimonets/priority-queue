9aacff0a5529aa3f3941eb122a21cf87893fb347
vzanimonets <vzanimonets@gmail.com>
2019-03-24 09:48:39 +0300

    feat: implementation of constructor, appendChild, remove functions

--- src/node.js
+++ src/node.js
@@ -1,23 +1,48 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	}
+  appendChild(node) {
+    if (this.left && this.right) {
+      return;
+    }
+    if (this.left) {
+      this.right = node;
+      this.right.parent = this;
+    } else {
+      this.left = node;
+      this.left.parent = this;
+    }
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    if (node === this.left) {
+      this.left = null;
+      node.parent = null;
+    } else if (node === this.right) {
+      this.right = null;
+      node.parent = null;
+    } else {
+      throw "Error, passed node is not a child of this node";
+    }
+  }
 
-	}
+  remove() {
+    if (this.parent != null) {
+      this.parent.removeChild(this);
+    }
+  }
 
-	removeChild(node) {
-
-	}
-
-	remove() {
-
-	}
-
-	swapWithParent() {
-		
-	}
+  swapWithParent() {
+    if ((this.parent = !null)) {
+      
+    }
+  }
 }
 
 module.exports = Node;
